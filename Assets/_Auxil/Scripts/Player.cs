a0966fb7558d985f7af7f08173f9e76b9bf6ef4b
Mouadh Mkadmi <36245246+wvagen@users.noreply.github.com>
2024-01-27 09:14:21 +0100

    Version 1.2

--- Assets/_Auxil/Scripts/Player.cs
+++ Assets/_Auxil/Scripts/Player.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField]
+    private Rigidbody myRig;
+    [SerializeField]
+    private float moveSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,13 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        GetInput();
+    }
+
+    void GetInput()
+    {
+        float moveX = -Input.GetAxis("Horizontal");
+        myRig.velocity = transform.right * moveX * moveSpeed;
+        //transform.Translate(Vector2.right * Time.deltaTime * moveX * moveSpeed);
     }
 }
