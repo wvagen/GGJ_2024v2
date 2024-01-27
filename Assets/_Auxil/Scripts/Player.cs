ea760631862f7af2ee3d2b21968446031fe0bab1
Mouadh Mkadmi <36245246+wvagen@users.noreply.github.com>
2024-01-27 19:40:46 +0100

    Version 2.9

--- Assets/_Auxil/Scripts/Player.cs
+++ Assets/_Auxil/Scripts/Player.cs
@@ -21,10 +21,14 @@ public class Player : MonoBehaviour
     [SerializeField]
     private Camera mainCam;
 
+    public float offsetValue;
+
+    Vector3 hipInitRot;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        hipInitRot = Hip.localEulerAngles;
     }
 
     // Update is called once per frame
@@ -50,15 +54,20 @@ public class Player : MonoBehaviour
 
     void LookTowards()
     {
-        Vector3 mousePosition = Input.mousePosition;
+        //Vector3 mousePosition = Input.mousePosition;
 
-        // Convert the mouse position to a point in the game world
-        Vector3 worldMousePosition = mainCam.ScreenToWorldPoint(new Vector3(mousePosition.x, mousePosition.y, 10f));
+        //// Convert the mouse position to a point in the game world
+        //Vector3 worldMousePosition = mainCam.ScreenToWorldPoint(new Vector3(mousePosition.x, mousePosition.y, 10f));
 
-        //// Make the object look towards the mouse position
-        //Hip.LookAt(worldMousePosition, Vector3.right);
+        ////// Make the object look towards the mouse position
+        //Hip.LookAt(worldMousePosition, Vector3.up);
+        //Hip.localEulerAngles = new Vector3(hipInitRot.x, Hip.eulerAngles.y + offsetValue, hipInitRot.z);
 
-        Debug.Log((worldMousePosition - Hip.transform.position).normalized);
+        // Vector2 normolizedVec = (Hip.transform.position - worldMousePosition).normalized;
+        //// Debug.Log(normolizedVec);
+        // Debug.Log(Mathf.Acos(worldMousePosition.normalized.y) * Mathf.Rad2Deg);
+        // Debug.Log(Mathf.Acos(normolizedVec.y) * Mathf.Rad2Deg);
+        // Debug.DrawLine(Hip.position, worldMousePosition, Color.red);
 
         float moveY = -Input.GetAxis("Vertical");
         Hip.Rotate(Vector3.up * Time.deltaTime * rotSpeed * moveY);
